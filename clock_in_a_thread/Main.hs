1f7a17201c927a2e009738f4ff14511d27b21a5e
madjestic <madjestic13@gmail.com>
2014-07-14 20:39:35 +0200

    first commit

---
+++ clock_in_a_thread/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+-- | Here I attempt to create a clock process in a separate thread
+-- | that would send tick to the reactive-banana - based Event Network
+
+main :: IO ()
+main = do
+     print "hello, world!"
